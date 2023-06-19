2b12a3ad37377de5cd152f069e6a7a0e724cd560
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-06-19 11:31:15 +0530

    trash commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import { useState } from "react";
 import Form from "./components/Form";
 import TileMain from "./components/TileMain";
 
-
 const App = () => {
-  
-    const [data, setData] = useState([])    
+    const [data, setData] = useState([]);
     return (
         <div className="content">
             <Form data={data} setData={setData} />
-            <TileMain data={data}/>
+            <TileMain data={data} />
         </div>
     );
 };
